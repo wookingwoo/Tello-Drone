2ac5cb6be2d25d9835e6bb3dc14cf385386007e2
JeongHeon Woo <email@wookingwoo.com>
2022-11-25 02:51:33 +0900

    Add get_temperature

--- takeoff.py
+++ takeoff.py
@@ -3,7 +3,9 @@ import time
 
 me = Tello()
 me.connect()
-print(me.get_battery())  # 배터리 잔량 확인
+
+print(f"battery: {me.get_battery()}%")  # 배터리 잔량 출력
+print(f"temperature: {me.get_temperature()}°C")  # 드론 온도 출력
 
 me.takeoff()  # 기체 이륙
 time.sleep(1)
